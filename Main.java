ae7a1ca30d642556754ed2eb084c0e2e7e9f12a6
alejs <alejsant75@gmaill.com>
2022-11-23 13:43:07 -0600

    1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    int r = sumar(10, 12, 11);
+    }
+
+    public static int sumar(int a, int b, int c){
+        return a+b+c;
+    }
+}
